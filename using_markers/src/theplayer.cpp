e22ddc234677f434fbecc8210f2d99ef8bee6bd2
Goutham Miryala <goutham.miryala@ndsu.com>
2017-03-23 19:02:50 -0500

    added Fuzzlyogic pointtodestination

--- using_markers/src/theplayer.cpp
+++ using_markers/src/theplayer.cpp
@@ -11,7 +11,7 @@ commands theplayer::go2waypoint(double x, double y)
      //orient robot towards ball
 
     //float alpha = acos((myPOS.x-x)/pow (pow(myPOS.x-x,2)+pow(myPOS.y-y,2),0.5));
-    
+   /* 
 	double alpha;
 	double dx = x - myPOS.x;
 	double dy = y - myPOS.y;
@@ -30,33 +30,28 @@ commands theplayer::go2waypoint(double x, double y)
 {
         if (abs(alpha - myPOS.w) > 10) 
 	{
-		if (alpha > myPOS.w) return GOLEFT;
+		if (alpha > myPOS.w) return GOLEFT;	
 		else return GORGHT;
 	}
 	else return GOFWD;
 }
 
-   // if (myPOS.x < x +.4 ) //waypoint is to the right
-   /*{
-        double alpha = atan2((myPOS.y -y), (myPOS.x -x));
-	printf("player x = %f ball x = %f alpha :%f omega = %f\n", myPOS.x, x, alpha, myPOS.w);
-        if (alpha > myPOS.w+.4) return GORGHT; 
-        else if (alpha < myPOS.w-.4) return GOLEFT;
-        else return GOFWD;
-     }
-   else if (myPOS.x > x -.4) //waypoint is to the left
-    {
-        double alpha = -atan((myPOS.x -x)/(myPOS.y -y));
-	printf("player x = %f ball x = %f alpha :%f omega = %f\n", myPOS.x, x, alpha, myPOS.w);
-        if (alpha > myPOS.w +.4) return GOLEFT;
-        else if (alpha < myPOS.w -.4) return GORGHT;
-        else return GOFWD;
-    }*/
-    //else return GOFWD;
+ */
+            double dx = x - myPOS.x;
+            double dy = y - myPOS.y;
 
-    //if (alpha> myPOS.w + .1) return GOLEFT;
-   // else if (alpha < myPOS.w -.1) return GORGHT;
-    ///else return GOFWD;
+            double lAngle = atan2(dy, dx);
+
+            lAngle = abs(lAngle * 180 / PI);
+
+            if ((dx >= 0) && (dy >= 0)) { lAngle = 360 - lAngle; }
+            if ((dx < 0) && (dy >= 0)) { lAngle = 270 - (lAngle - 90); }
+	
+                      
+           // return abs(lAngle);     
+
+		return GOFWD;     
+        
 
     //go towards ball
 }
