ffd1db6832f5dc36713ddb0cf5a0e57fe5d843a5
Lou <DangerousElectrician@safeelectrons.net>
2017-05-07 17:51:32 -0500

    try toggling

--- serialtest/sertest.cpp
+++ serialtest/sertest.cpp
@@ -5,24 +5,24 @@
 int main() {
 	int fd = init_bots("/dev/ttyUSB0");
 	std::cout << fd <<std::endl;
-	int8_t d = 127;
+	int8_t d = 0;
 	while(1) {
 		//command_bot(1481622, d, d);
-		d = 120;
+		d = 10;
 		command_bot(1557094, d, -d);
 		usleep(10000);
 		command_bot(1554593, d, -d);
 		usleep(10000);
 		command_bot(1481416, d, -d);
-		usleep(1000000);
+		usleep(10000);
 
-		d = 0;
-		command_bot(1557094, d, d);
-		usleep(10000);
-		command_bot(1554593, d, d);
-		usleep(10000);
-		command_bot(1481416, d, d);
-		usleep(1000000);
+		//d = 0;
+		//command_bot(1557094, d, d);
+		//usleep(10000);
+		//command_bot(1554593, d, d);
+		//usleep(10000);
+		//command_bot(1481416, d, d);
+		//usleep(1000000);
 		//1554593
 		//1481416
 	}
